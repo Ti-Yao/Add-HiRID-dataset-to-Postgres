9da9aba32bb0d97fadccd4b0e0d4450cc1efec2a
Tina Yao <74569724+Ti-Yao@users.noreply.github.com>
2021-06-04 12:02:08 +0100

    Add files via upload

---
+++ postgres_check.sql
@@ -0,0 +1,25 @@
+with expected as
+(
+select 'observations_table' as tbl, 30514 as row_count UNION ALL 
+select 'pharma_records' as tbl, 16270399 as row_count 
+
+)
+, observed as
+(
+select 'observations_table' as tbl, count(*) as row_count from observations_table UNION ALL 
+select 'pharma_records' as tbl, count(*) as row_count from pharma_records
+
+)
+select
+exp.tbl
+, exp.row_count as expected_count
+, obs.row_count as observed_count
+, case
+	when exp.row_count = obs.row_count
+	then 'PASSED'
+	else 'FAILED'
+	end as ROW_COUNT_CHECK
+from expected exp
+inner join observed obs
+	on exp.tbl=obs.tbl
+	order by exp.tbl;
